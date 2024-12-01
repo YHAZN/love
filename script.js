1166d4e19ac2bce9cdfd72a1acf2f2ac9199df74
Hanzhe Yan <30371934+YHAZN@users.noreply.github.com>
2024-12-01 15:35:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let currentSlide = 0;
 
 function openCard() {
     const card = document.querySelector('.card');
@@ -7,3 +8,23 @@ function openCard() {
 function sendLove() {
     alert('Love sent with all my heart! ❤️');
 }
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.carousel-images img');
+    const totalSlides = slides.length;
+    currentSlide = (index + totalSlides) % totalSlides; // Ensure looping
+    document.querySelector('.carousel-images').style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+// Automatically transition slides every 3 seconds
+setInterval(() => {
+    nextSlide();
+}, 3000);
